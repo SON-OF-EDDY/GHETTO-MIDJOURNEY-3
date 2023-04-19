56d21f40182e3d14a50c1122f18e7f56a45f6fe5
tim <timohide13@gmail.com>
2023-04-19 15:04:07 +0300

    final

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
 import requests
@@ -11,10 +12,9 @@ version = model.versions.get("db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c
 def process_query(request):
     if request.method == 'POST':
         data = json.loads(request.body)
-        #query = data['query']
-        #new_image_url = generate_image(query=query)[0]
-        #json_data = json.dumps(new_image_url)
-        json_data='testing'
+        query = data['query']
+        new_image_url = generate_image(query=query)[0]
+        json_data = json.dumps(new_image_url)
         return HttpResponse(json_data, content_type='application/json')
 
 def generate_image(query):
